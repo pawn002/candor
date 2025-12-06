fc86be65818f6e7c123e92d60e7885baee0f2d61
John R <24492257+pawn002@users.noreply.github.com>
2025-12-05 22:24:17 -0500

    fix: add @storybook/addon-docs and update check-mcps command
    - Install @storybook/addon-docs@10.1.4 to fix autodocs renderer error
    - Replace @storybook/addon-essentials with @storybook/addon-docs in Storybook config
    - Update /check-mcps command to automatically free browser resource locks
    - Resolves 'docsParameter.renderer is not a function' console error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-a11y", "@chromatic-com/storybook"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@chromatic-com/storybook",
+  ],
   framework: {
     name: "@storybook/angular",
     options: {},
